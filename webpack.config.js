0960b42c4dd8f079078fadb899c454e5c04c5ee0
firstpj <114886262+firstpj@users.noreply.github.com>
2023-06-16 10:55:54 +0100

    dist/index.js, src files and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
         index: './src/index.js'
     },
     output: {
-        filename: '[name].js',
+        filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
