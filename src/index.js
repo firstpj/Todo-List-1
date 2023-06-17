4110786f73dccc808c8ce15375926cc40e66cf18
firstpj <114886262+firstpj@users.noreply.github.com>
2023-06-17 15:37:52 +0100

    .gitignore, dist/index.bunle.js files

--- src/index.js
+++ src/index.js
@@ -1,56 +1,49 @@
-import'./src/index.css';
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+import './style.css';
+
+// Get DOM
+const listElement = document.createElement('div');
+listElement.classList = 'dynamic-container';
+const listContainer = document.querySelector('.list');
+
+const task = [
+  {
+    description: 'task1',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'task2',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'task3',
+    completed: false,
+    index: 4,
+  },
+  {
+    description: 'task4',
+    completed: false,
+    index: 3,
+  },
+];
+
+function iterateTasks() {
+  const sortedTasks = task.sort((a, b) => a.index - b.index);
+  sortedTasks.forEach((item) => {
+    listElement.innerHTML
+        += `
+        <div class="list-properties">
+            <span class="left-items">
+                <input type="checkbox">
+                <p>${item.description}</p>
+            </span>
+            <i class="fa-solid fa-ellipsis-vertical"></i>
+        </div>
+        <hr>
+        `;
+  });
+
+  listContainer.appendChild(listElement);
 }
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert(" write something !");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
-}
+iterateTasks();
